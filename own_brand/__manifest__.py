0ccf06e62bb6c84620acc7d1eb1d71dcd29f6d81
nomadictech-org <ramtekedinesh00@gmail.com>
2026-05-13 20:04:53 +0530

    [Update]: Fix button splash and background colour and others

--- own_brand/__manifest__.py
+++ own_brand/__manifest__.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'NomadicTech Own Brand',
+    'name': 'Own Brand',
     'version': '18.0.1.0.0',
     'category': 'Themes/Backend',
-    'summary': 'Modern Odoo 18 Branding for NovaOS and Dubak Projects',
+    'summary': 'Modern Odoo 18 Branding for Nordivs Style',
     'author': 'NomadicTech',
-    'website': 'https://nomadictech.com',  # Example website
+    'website': 'https://nordivs.com',  # Example website
     'license': 'LGPL-3',
     'depends': [
+        'base',
         'web',
         'mail',  # Required for the unified <chatter/> and mail templates
+        'crm',
     ],
     'data': [
         'views/res_company_view.xml',
@@ -18,13 +20,11 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'own_brand/static/src/scss/appbar.scss',
             'own_brand/static/src/scss/menu_item.scss',
             "own_brand/static/src/scss/button_style.scss",
             'own_brand/static/src/scss/files_attached.scss',
             'own_brand/static/src/scss/ios_cards.scss',
             'own_brand/static/src/scss/ios_kanban.scss',
-            'own_brand/static/src/scss/layout_colors.scss',
             'own_brand/static/src/scss/modal.scss',
             'own_brand/static/src/scss/status_bar.scss',
             'own_brand/static/src/scss/tabs.scss',
@@ -34,6 +34,8 @@
             # 'own_brand/static/src/scss/login.scss',
         ],
     },
+    'post_init_hook': 'post_init_hook',
+    'post_migrate_hook': 'post_migrate_hook',
     'installable': True,
     'application': True,  # Set to True if this is the primary "NovaOS" entry point
 }
